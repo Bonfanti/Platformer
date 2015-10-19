65b11925479fe76ee052701b740cddac03776f2e
unknown <student@CSE3-169.cse.asmsa.org>
2015-10-19 17:25:02 -0500

    Wild Bill: use command 'git add --all' then the rest

--- Jumpy Squirrel/Assets/scripts/SpawnPlatforms.cs
+++ Jumpy Squirrel/Assets/scripts/SpawnPlatforms.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class SpawnPlatforms : MonoBehaviour {
 
 	public Transform platform;
+	public Transform c;
+	public Transform backdrop;
+	private float prevValue;
+	private bool left;
 	// Use this for initialization
 	void Start () {
 		if(platform.position.x == -1.88F){
@@ -16,6 +20,15 @@ public class SpawnPlatforms : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown ("left")) left = true;
+		if (Input.GetKeyUp ("left"))
+			left = false;
+		if (Mathf.Abs (prevValue - (c.position.x % 5.14F)) > 5 ) {
+			int sign = 4;
+			if(left) sign = -4;
+			Vector3 pos2 = new Vector3((float)(c.position.x + sign + 5.14F), 1.5F, 10.0F);
+			Instantiate(backdrop, pos2, this.transform.rotation);
+		}
+		else prevValue = c.position.x % 5.14F;
 	}
 }
